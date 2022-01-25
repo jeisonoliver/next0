48da60e268f870a4e0dae7ebb5157b202058b977
jeisonoliver <jeisonjs42@gmail.com>
2022-01-25 20:45:17 -0300

    first commit

---
+++ pages/paginaHino.js
@@ -0,0 +1,17 @@
+import { hino } from "./hino";
+
+export default function Main(){
+
+    return (
+ 
+       <div>
+ 
+          <h1>Hino do Flamengo</h1>
+ 
+          <hino/>
+ 
+       </div>
+ 
+    )
+ 
+ }
